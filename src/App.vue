ddb9e7549f58eba6d6b9592520c5fc6b0d27cb93
CaptainFulminyx <captainfulminyx@gmail.com>
2026-01-25 18:35:24 +0000

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
-<script setup>
-</script>
 <template>
+  <div id="app">
+    <DoodlePortfolio />
+  </div>
 </template>
-<style scoped>
+
+<script setup>
+import DoodlePortfolio from './components/Site.vue'
+</script>
+
+<style>
+/* Import Font Awesome and Google Fonts */
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css');
+@import url('https://fonts.googleapis.com/css2?family=Indie+Flower&family=Poppins:wght@300;400;500;600;700&display=swap');
+
+/* Reset and base styles */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+}
 </style>
